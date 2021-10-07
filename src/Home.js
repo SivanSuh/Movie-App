645b32c556f7d42d4c6de6a3013593a3fcf1157b
SIVAN SUH <sivansuh.is@gmail.com>
2021-10-07 22:32:43 +0300

    movie-api-app

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchFrom from "./SearchForm"
+import Movies from "./Movies"
+const Home = () => {
+    return (
+        <main>
+            <SearchFrom />
+            <Movies />
+        </main>
+    )
+}
+
+export default Home
